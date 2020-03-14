d9932801d0e2d2ae48a4c283f4e9ef07f5a3f0d3
Jeff Charron <jeff.charron@coxautoinc.com>
2020-03-14 11:55:41 -0400

    fetching taco data

--- src/App.js
+++ src/App.js
@@ -7,9 +7,22 @@ function App() {
 
   const [tacoData, setTacoData] = useState();
   const [location, setLocation] = useState();
+  const [loading, setLoading] = useState(false);
 
   const handleTacoFormSubmit = (e) => {
     e.preventDefault();
+    setLoading(true);
+    fetch(`https://colorful-halibut.glitch.me/api/v1/businesses/search?location=${location}&term=tacos`)
+    .then((response) => {
+      if(!response.ok) {
+        throw Error(response.statusText);
+      }
+      setLoading(false)
+      return response
+    })
+    .then(response => response.json())
+    .then(response => setTacoData(response))
+    .catch();
   };
 
   const handleInputChange = (e) => {
@@ -20,7 +33,13 @@ function App() {
     <div className="App container-fliud">
       <div className="row justify-content-center">
         <div className="col col-sm-9 col-md-7 col-lg-4 p-5">
-          <LocationForm handleTacoFormSubmit={handleTacoFormSubmit} handleInputChange={handleInputChange} />
+          { !tacoData && !loading &&
+            <LocationForm handleTacoFormSubmit={handleTacoFormSubmit} handleInputChange={handleInputChange} />
+          }{ loading &&
+            <h1>loading</h1>
+          }{ tacoData &&
+            JSON.stringify(tacoData)
+          }       
         </div>
       </div>
     </div>
