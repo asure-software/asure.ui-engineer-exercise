e306e61212174cf2cd14759e881db6d8690838bf
SaraRitter <43254917+SaraRitter@users.noreply.github.com>
2020-11-18 01:17:35 -0500

    graphics

--- taco-frontend/src/LandingPage/LandingPage.js
+++ taco-frontend/src/LandingPage/LandingPage.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { TopNav } from './TopNav/TopNav';
+import taco from '../assets/taco.png';
+import styles from './LandingPage.module.css';
 
 export function LandingPage() {
     return (
         <div>
-            <TopNav/>
+            <TopNav />
+            <img src={taco} className={styles.taco} alt='taco'/>
         </div>
     );
 }
