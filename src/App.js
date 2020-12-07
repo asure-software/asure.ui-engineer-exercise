6753a3db4a93f4c91f8f281ed77971e947ee0324
Kento Kawakami <kawakamikento@gmail.com>
2020-12-06 19:50:19 -0500

    Created static HTML

--- src/App.js
+++ src/App.js
@@ -1,7 +1,65 @@
 import "./styles/main.scss";
+import taco from "./assets/taco.gif";
+import yelp from "./assets/yelp.png";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <main>
+      <article>
+        <img src={taco} alt="animation of a taco cat with legs" />
+        <header>
+          <h1>
+            Spec-<span>TACO</span>-lar Restaurants!
+          </h1>
+          <h4>in...</h4>
+        </header>
+        <input placeholder="Enter a location (city/zip)" />
+        <button>What's the taco the town?</button>
+        <footer>
+          <button>
+            <i className="fas fa-angle-down"></i>
+          </button>
+        </footer>
+      </article>
+      <article>
+        <nav>
+          <button>Best Match</button>
+          <button>Rating</button>
+          <button>Reviews</button>
+          <button>Distance</button>
+        </nav>
+        <ul className="results">
+          <li>
+            <div className="information-container">
+              <div className="information">
+                <h2>
+                  Tacobell <span>(3 reviews)</span>
+                </h2>
+                <h3>
+                  <span>open</span> $
+                </h3>
+                <div className="more-information">
+                  <div>
+                    <a href="http://maps.google.com/?q=51 davis blvd apt A tampa, FL 33606">
+                      51 Davis Blvd Apt A <br />
+                      Tampa, FL 33606
+                      <br />
+                    </a>
+                    <a href="tel:(813)334-6678">(813)334-6678</a>
+                  </div>
+                  <img src={yelp} alt={`Yelp review for Tacobell`} />
+                </div>
+              </div>
+              <div className="rating">5.0</div>
+            </div>
+            <button>
+              <i className="fas fa-angle-down"></i>
+            </button>
+          </li>
+        </ul>
+      </article>
+    </main>
+  );
 }
 
 export default App;
